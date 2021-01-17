b26d8e59bbde051607c82d48d14bd5d5b39ca89d
“chandan” <nerdy410@gmail.com>
2021-01-17 07:14:22 +0530

    added one file and modified another

---
+++ src/main/java/pack1/CheckGIT_2ndRound.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class CheckGIT_2ndRound {
+	
+	//CheckGIT_2ndRound
+	//code line added newly
+	//code added newly
+
+}
--- src/main/java/pack1/NewFrmGitHub.java
+++ src/main/java/pack1/NewFrmGitHub.java
@@ -1,2 +1,4 @@
 //This is new test case from GitHub
 //Peace
+//2nd Round of checking from local to remote
+//code added newly
